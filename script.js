a4969fca36c0932d5f05c26826f613b23b3b2d51
yagyam <vishuaggarwal219@gmail.com>
2024-09-14 12:47:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//for Navigation Links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
