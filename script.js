0a1aa2d215cc98658011af3d6e224b6a302191d4
yagyam <vishuaggarwal219@gmail.com>
2025-08-27 21:42:02 +0530

    Updated with AI and ML skills But still projects are left

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+alert('Hello, The site is bit outdated. Projects section is not updated yet. For the latest projects, Please see the Resume on this site. Thank You!');
+alert("I'll be updating the site soon. Please bare with it!");
 //for Navigation Links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
